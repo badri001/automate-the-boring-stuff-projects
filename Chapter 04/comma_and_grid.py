14baea1331048b3a15a895b0b5b866f066527e02
BadRi.SR <34916335+badri001@users.noreply.github.com>
2021-01-09 23:37:41 +0530

    Update comma_and_grid.py

--- Chapter 04/comma_and_grid.py
+++ Chapter 04/comma_and_grid.py
@@ -17,6 +17,34 @@ def list_joiner(list):
 
 print(list_joiner(spam))
 
+''' Random flip coins '''
+
+
+import random
+data=[]
+count=0
+numberOfStreaks=0
+for exprimentNumber in range(10000):
+    for i in range(100):
+        res=random.randint(0,1)
+        if res==0:
+            data.append('H')
+        else :
+            data.append('T')
+i=0
+for value in data :
+    
+    if data[int(int(i)-1)]==data[i]:
+        count+=1
+    elif count==6:
+        numberOfStreaks+=1
+        count=0
+    else:
+        count=0
+    i+=1
+print('Chance of streak: %s%%' % (numberOfStreaks / 100))
+
+
 
 """Character Picture Grid"""
 
